803965b292b9ba1924775fedcd914391b95b329c
PedroyDev <130869544+PedroyDev@users.noreply.github.com>
2023-04-15 18:28:13 -0300

    commit social

---
+++ src/main/java/com/escola/escola/Escola.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.escola.escola;
+
+/**
+ *
+ * @author DiogoMelo
+ */
+public class Escola {
+
+    public static void main(String[] args) {
+        
+         Sala room = new Sala();
+       room.pack();
+       room.setVisible(true);
+       
+
+    }
+}
